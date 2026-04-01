ae9349fe0d868bb65aceed7b1b8dccf5718db362
Gabriel Théorêt <2538532@cegeplimoilou.ca>
2026-04-01 09:04:15 -0400

    ajout des bon fichiers FINALEMENT -_-

---
+++ APP4/src/electronique/Circuit.java
@@ -0,0 +1,11 @@
+package electronique;
+
+import java.util.List;
+
+abstract class Circuit extends Composant {
+    protected List<Composant> composants;
+
+    public Circuit(List<Composant> composants){
+        this.composants = composants;
+    }
+}
