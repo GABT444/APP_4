fe47312b7cfd3e49ed77a09fa8169ef8436ce7fd
2538532 <2538532@cegeplimoilou.ca>
2026-04-01 10:10:17 -0400

    circuits builder ajoute lireCircuitsBuilder(fin) -_-

--- untitled/src/app/CircuitBuilder.java
+++ untitled/src/app/CircuitBuilder.java
@@ -1,15 +1,56 @@
 package app;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import src.electronique.CircuitParallele;
+import src.electronique.CircuitSerie;
+import src.electronique.Composant;
 import src.electronique.Resistance;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CircuitBuilder {
-    private CircuitBuilder lireCircuitBuilder(JsonNode node){
+    public CircuitBuilder() {
+
+    }
+
+    public Composant construireCircuit(String node) {
+        List<Composant> Circuits = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            JsonNode donneescircuits = mapper.readTree(new File(node));
+            JsonNode e = donneescircuits.get("circuit");
+            Composant composant = lireCircuitBuilder(e);
+            return composant;
+        } catch (IOException e) {
+            System.err.println("erreur de lecture : " + e.getMessage());
+            ;
+        }
+        throw new IllegalArgumentException("le fichier ne peut pas être lu");
+    }
+
+    private Composant lireCircuitBuilder(JsonNode node) {
         String type = node.get("type").asText();
 
-        if ("resistance".equals(type)){
+        if ("resistance".equals(type)) {
             return new Resistance(node.get("valeur").asDouble());
-
+        } else if ("serie".equals(type)) {
+            List<Composant> composants = new ArrayList<>();
+            for (JsonNode composantNode : node.get("composants")) {
+                composants.add(lireCircuitBuilder(composantNode));
+            }
+            return new CircuitSerie(composants);
+        } else if ("parallele".equals(type)) {
+            List<Composant> composants = new ArrayList<>();
+            for (JsonNode composantNode : node.get("composants")) {
+                composants.add(lireCircuitBuilder(composantNode));
+            }
+            return new CircuitParallele(composants);
         }
+        throw new IllegalArgumentException("type de circuits : " + type);
     }
 }
--- untitled/src/src/electronique/Composant.java
+++ untitled/src/src/electronique/Composant.java
@@ -1,6 +1,6 @@
 package src.electronique;
 
-abstract class Composant {
+public abstract class Composant {
     public Composant(){
 
     }
