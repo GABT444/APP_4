b61b44e3a6f51e1ef73c8d984c62017bd6136a36
2538532 <2538532@cegeplimoilou.ca>
2026-04-08 09:13:59 -0400

    continuer le main et ajoute de méthode pour simplifier le main et ajoute de vérification pour le circuits parallele

--- untitled/src/app/CircuitApp.java
+++ untitled/src/app/CircuitApp.java
@@ -1,27 +1,36 @@
 package app;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class CircuitApp {
-    public CircuitApp(){
+    public CircuitApp() {
 
     }
 
     static void main(String[] args) {
- String directoryPath = "u:\\Users\\APP4-gr1\\app 4\\APP_4\\untitled";
-    File drectory = new File(directoryPath);
-    File[] files = drectory.listFiles();
-    boolean valide = false;
-     while(valide == false){
-         System.out.println("Bienvenue, ce programme est a l'utilité de calculer la resistance dans les circuits suivant:");
-         for(int i = 0; i < files.length; i++){
-             System.out.println("-"+i +"."+files[i].getName());
-         }
-         System.out.println("écriver le nombre associé au circuits suivant pour savoir sa resistance ou N pour quitter le programme");
-
-     }
+        String directoryPath = "u:\\Users\\APP4-gr1\\app 4\\APP_4\\untitled";
+        File drectory = new File(directoryPath);
+        File[] files = drectory.listFiles();
+        int reponse_int= -1;
+        String reponse_String ="";
+        Scanner sc = new Scanner(System.in);
+        while (reponse_int == -1 || !reponse_String.equals("n")) {
+            System.out.println("Bienvenue, ce programme est a l'utilité de calculer la resistance dans les circuits suivant:");
+            for (int i = 0; i < files.length; i++) {
+                System.out.println("-" + i + "." + files[i].getName());
+                try {
+                    reponse_int = Integer.parseInt(sc.nextLine());
+                } catch (Exception e) {
+                    reponse_String = sc.nextLine();
+                    reponse_String.toLowerCase();
+                }
+            }
+            System.out.println("écriver le nombre associé au circuits suivant pour savoir sa resistance ou N pour quitter le programme");
+        }
     }
 
-    }
+    private File[]
+}
 
--- untitled/src/src/electronique/CircuitParallele.java
+++ untitled/src/src/electronique/CircuitParallele.java
@@ -11,9 +11,16 @@ public class CircuitParallele extends Circuit {
     public double calculerResistance() {
         double resistance = 0;
         for (Composant c : composants) {
+            valider(c);
             resistance += 1/c.calculerResistance();
 
         }
         return resistance;
+    }
+
+    private void valider(Composant c) {
+        if (c.calculerResistance()==0){
+            throw new IllegalArgumentException("il ne peut être 0 pour sa resistance"):
+        }
     }
 }
