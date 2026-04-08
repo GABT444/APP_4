9e811330d368e70a5a540c3758a5d184c122047e
2538532 <2538532@cegeplimoilou.ca>
2026-04-08 08:54:47 -0400

    ajoute du main fonctionnel :)

--- untitled/src/app/CircuitApp.java
+++ untitled/src/app/CircuitApp.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class CircuitApp {
@@ -7,9 +8,19 @@ public class CircuitApp {
 
     }
 
-    static void main() {
-        boolean valide= false;
-        while ()
+    static void main(String[] args) {
+ String directoryPath = "u:\\Users\\APP4-gr1\\app 4\\APP_4\\untitled";
+    File drectory = new File(directoryPath);
+    File[] files = drectory.listFiles();
+    boolean valide = false;
+     while(valide == false){
+         System.out.println("Bienvenue, ce programme est a l'utilité de calculer la resistance dans les circuits suivant:");
+         for(int i = 0; i < files.length; i++){
+             System.out.println("-"+i +"."+files[i].getName());
+         }
+         System.out.println("écriver le nombre associé au circuits suivant pour savoir sa resistance ou N pour quitter le programme");
+
+     }
     }
 
     }
